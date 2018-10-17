7d743d1a1727da30f3e5895703a2700ee95fa273
MichaelPerkins2 <michael.perkins.d@gmail.com>
2018-10-17 14:01:17 -0600

    navbar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,11 +10,18 @@ class NavBar extends Component {
     render() {
         return (
             <div>
-                <header className='header'>
+                <header className='NavBar'>
                     <div className='container-fluid'>
                         <div className='row'>
-                            <a className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' href='/'><i className='drop-down fas fa-bars'></i></a>
-                            <Button className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-32' bsStyle='link'  href='/'>Title</Button>
+                            <div className="dropdown">
+                            <a className='dropbtn col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' href='/'><i className='drop-down fas fa-bars'></i></a>
+                                <div className="dropdown-content">
+                                    <a href="#">Featured</a>
+                                    <a href="#">Popular</a>
+                                    <a href="#">New Releases</a>
+                                </div>
+                            </div>
+                            <Button className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-32' bsStyle='link'  href='/'>MGR</Button>
                             <a className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' href='/' id=''><i className='search fas fa-search'></i></a>
                             <Button className='login-btn col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' bsSize='default' bsStyle='primary' href='/login'>Sign In</Button>
                         </div>

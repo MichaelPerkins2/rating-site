88e9eaeb6c1a70dacfdf2c9fdc4fdd03f875c5fb
MichaelPerkins2 <michael.perkins.d@gmail.com>
2018-10-22 13:53:57 -0600

    nav-bar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,21 +11,23 @@ class NavBar extends Component {
         return (
             <div>
                 <header className='NavBar'>
-                    <div className='container-fluid'>
-                        <div className='row'>
-                            <div className="dropdown">
-                            <a className='dropbtn col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' href='/'><i className='drop-down fas fa-bars'></i></a>
+
+                        <div className='nav-row row'>
+                            <div className='dropdown'>
+                            <a className='dropbtn' href='/'><i className='drop-down fas fa-bars'></i></a>
                                 <div className="dropdown-content">
-                                    <a href="#">Featured</a>
-                                    <a href="#">Popular</a>
-                                    <a href="#">New Releases</a>
+                                    <a href='#'>Featured</a>
+                                    <a href='#'>Popular</a>
+                                    <a href='#'>New Releases</a>
+                                    <a href='#'>Sign In</a>
+                                    <a href='#'>Search</a>
                                 </div>
                             </div>
-                            <Button className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-32' bsStyle='link'  href='/'>MGR</Button>
-                            <a className='col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' href='/' id=''><i className='search fas fa-search'></i></a>
-                            <Button className='login-btn col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' bsSize='default' bsStyle='primary' href='/login'>Sign In</Button>
+                            <Button className='page-title col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-2' bsStyle='link'  href='/'>MGR</Button>
+                            <a className='' href='/' id=''><i className='search fas fa-search'></i></a>
+                            <Button className='login-btn col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-2' bsSize='default' bsStyle='primary' href='/login'>Sign In</Button>
                         </div>
-                    </div>
+
                 </header>
 
             </div>
